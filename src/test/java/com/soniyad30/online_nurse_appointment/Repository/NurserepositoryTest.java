06f716230c8d60e357eea51dcf95c7e9a4542056
soniya737 <soniyadhakad13099@gmail.com>
2023-08-25 17:10:28 +0530

    first testing commit

--- src/test/java/com/soniyad30/online_nurse_appointment/Repository/NurserepositoryTest.java
+++ src/test/java/com/soniyad30/online_nurse_appointment/Repository/NurserepositoryTest.java
@@ -1,6 +1,36 @@
 package com.soniyad30.online_nurse_appointment.Repository;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import com.soniyad30.online_nurse_appointment.Entity.NurseEntity;
+@SpringBootTest
 public class NurserepositoryTest {
-    
+    @Test
+
+    void NurseRepositorySaveReturnSavesNurse()
+    {
+        NurseEntity nurse =NurseEntity.builder().nurseName("").nurseAddress("").nurseContactNo("").build();
+
+        NurseEntity savedNurse = nurse_Repository.save(nurse);
+
+        Assertions.assertThat(savedNurse).isNotNull();
+
+    }
+
+    @Test
+    void NurseRepositorySaveIncrementNurseID()
+    {
+        NurseEntity nurse =NurseEntity.builder().nurseName("").nurseAddress("").nurseContactNo("").build();
+
+        NurseEntity savedNurse = nurse_Repository.save(nurse);
+
+        Assertions.assertThat(savedNurse.getNurseID()).isGreaterThan(0);
+
+    }
+
+    @Autowired
+    private Nurse_Repository nurse_Repository;
 }
